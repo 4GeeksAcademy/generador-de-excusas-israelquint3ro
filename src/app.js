857cd82b96c115f51c81c1f1ec2954ba244273b4
Israel Quintero <ijsalasar@hotmail.com>
2025-10-22 20:40:44 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+let who = ['My cat','My brother','My dog','My dad'];
+let action = ['ruined','ate','destroyed','broke'];
+let what = ['my homework','my car', 'my computer','the internet cable'];
+let when = ['when i was cooking', 'while i was coding','before my exam', 'after i finshed school'];
+
+
+function randomIndex(arr) {
+  let newIndex = Math.floor(Math.random()*arr.length);
+  return arr[newIndex];
+}
+
+window.onload = function (){
+let randomWho = randomIndex (who);
+let randomAction = randomIndex(action);
+let randomWhat = randomIndex(what);
+let randomWhen = randomIndex(when);
+
+
+const newExcuse = ` ${randomWho} ${randomAction} ${randomWhat} ${randomWhen} `; 
+
+document.getElementById ("excuse").innerHTML = newExcuse;
+}
+
