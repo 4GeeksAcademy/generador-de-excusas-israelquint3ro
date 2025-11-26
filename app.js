3aacc626fe46721a9df984fc8766a9019b1d837a
Israel Quintero <ijsalasar@hotmail.com>
2025-11-26 11:42:29 +0000

    probando

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
+window.onload = function () {
+    let who = ['my cat', 'my cousin', 'my dog', 'my gradnmother']
+    let action = ['ruined', 'ate', 'destroyed', 'broke']
+    let what = ['my homework', 'my car', 'my computer', 'the internet cable']
+    let when = ['When i was cooking', 'While i was coding', 'Before my exam', 'After i finshed school']
 
 
-let who = ['my cat','my brother','my dog','my dad']
-let action = [ 'ruined','ate','destroyed','broke']
-let what = ['my homework','my car', 'my computer','the internet cable']
-let when = ['When i was cooking', 'While i was coding','Before my exam', 'After i finshed school']
+
+    function excusesList() {
+
+        let randomWho = who[Math.floor(Math.random() * who.length)];
+        let randomAction = action[Math.floor(Math.random() * action.length)];
+        let randomWhat = what[Math.floor(Math.random() * what.length)];
+        let randomWhen = when[Math.floor(Math.random() * when.length)];
 
 
+        let randomExcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
 
-function excusesList() {
+        document.getElementbyId("excuse").textContent = randomExcuse;
 
- let randomWho= who[Math.floor(Math.random()*who.length)];
- let randomAction = action[Math.floor(Math.random()* action.length)];
- let randomWhat= what[Math.floor(Math.random()* what.length)];
- let randomWhen= when[Math.floor(Math.random()* when.length)];
+    }
+    excusesList()
 
- 
- let randomExcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
-
- document.getElementbyId("excuse").textContent = randomExcuse;
-
+    console.log(excusesList())
 }
-excusesList()
+console.log('Hola patricia')
+
+// let who = ['my cat', 'my cousin', 'my dog', 'my gradnmother']
+// let action = ['ruined', 'ate', 'destroyed', 'broke']
+// let what = ['my homework', 'my car', 'my computer', 'the internet cable']
+// let when = ['When i was cooking', 'While i was coding', 'Before my exam', 'After i finshed school']
+
+
+
+// function excusesList() {
+
+//     let randomWho = who[Math.floor(Math.random() * who.length)];
+//     let randomAction = action[Math.floor(Math.random() * action.length)];
+//     let randomWhat = what[Math.floor(Math.random() * what.length)];
+//     let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+
+//     let randomExcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+//     document.getElementbyId("excuse").textContent = randomExcuse;
+
+// }
+// excusesList()
+
+// console.log(excusesList())
